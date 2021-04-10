f8fc2edbebd0243c6cd348da41f4ca24ce05e649
DESKTOP-I8S375Q\kunhee <tha920@naver.com>
2021-04-10 12:33:05 +0900

    count  페이지 클릭이벤트 함수 이름 변경

--- pages/count.js
+++ pages/count.js
@@ -5,21 +5,21 @@ import { countMinusAction, countPlusAction } from "../reducers/count";
 
 const count = () => {
   const dispatch = useDispatch(); // dispatch를 사용하는 hook
-  const count = useSelector((state) => state.count); // store의 state 호출
+  const count = useSelector((state) => state.count); // store의 state 호출(reducers/index 참고)
 
-  const onClickPlus = useCallback(() => {
+  const handleClickPlus = useCallback(() => {
     dispatch(countPlusAction());
   }, []);
 
-  const onClickMinus = useCallback(() => {
+  const handleClickMinus = useCallback(() => {
     dispatch(countMinusAction());
   }, []);
 
   return (
     <div>
       카운트 : {count}
-      <button onClick={onClickPlus}>+</button>
-      <button onClick={onClickMinus}>-</button>
+      <button onClick={handleClickPlus}> + </button>
+      <button onClick={handleClickMinus}> - </button>
     </div>
   );
 };
