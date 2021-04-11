b76b06a8839177d78c8e136bf87d855dd9f170bc
DESKTOP-I8S375Q\kunhee <tha920@naver.com>
2021-04-11 12:25:37 +0900

    saga, thunk 기본 설정

--- sagas/index.js
+++ sagas/index.js
@@ -1,6 +1,16 @@
 import { all, call } from "redux-saga/effects";
 import saga from "./saga";
-
+/**
+ * 제너레이터 문법은 리턴을 순차적으로 시켜주는 것처럼 사용 할 수 있다.(yield)
+ *
+ */
 export default function* rootSaga() {
   yield all([call(saga)]);
 }
+
+/**
+ * react관련 어떤것들을 더 공부할 수 있느냐
+ * context api, mobx,
+ * immer.js(불변성 관리), typescript,
+ * graphql
+ */
